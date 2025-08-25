884f85530b000a635c24d6564b06c56ceffb4650
sikandrahere <148213515+sikandrahere@users.noreply.github.com>
2025-08-25 08:37:40 +0530

    cors origin change

--- server/src/lib/cors.js
+++ server/src/lib/cors.js
@@ -4,7 +4,7 @@ import initMiddleware from './init-middleware.js';
 
 const cors = initMiddleware(
   Cors({
-    origin: ['http://localhost:5173', 'https://email-analyzer-five.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://email-analyzer-five.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   })
